85287fc202776930df10413cabc3280295f42859
Anton Komissarov <akomissarov@zedat.fu-berlin.de>
2016-04-30 18:10:48 +0200

    a safer way to read files

--- apps/LAGAN/LAGAN.cpp
+++ apps/LAGAN/LAGAN.cpp
@@ -10,9 +10,18 @@
 using namespace seqan;
 
 int main() {
-
-    SeqFileIn sfi1(toCString(getAbsolutePath("apps/LAGAN/bovine_adenovirus_6.fa")));
-    SeqFileIn sfi2(toCString(getAbsolutePath("apps/LAGAN/bovine_adenovirus_D.fa")));
+    CharString filePath1 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_6.fa");
+    CharString filePath2 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_D.fa");
+    SeqFileIn sfi1;
+    if ( !open( sfi1, toCString(filePath1) ) ) {
+        std::cerr << "ERROR: Could not open the first file" << std::endl;
+        return 1;
+    }
+    SeqFileIn sfi2;
+    if ( !open( sfi2, toCString(filePath2) ) ) {
+        std::cerr << "ERROR: Could not open the second file" << std::endl;
+        return 1;
+    }
     CharString meta;
     Dna5String d5s1;
     Dna5String d5s2;
