faec6e01fcc5d000df799db4e56b8692d7728c72
Anton Komissarov <akomissarov@zedat.fu-berlin.de>
2016-04-30 17:31:43 +0200

    skeleton for lagan app

---
+++ apps/LAGAN/LAGAN.cpp
@@ -0,0 +1,14 @@
+//
+// Created by abadd0n on 4/30/16.
+//
+
+#include <iostream>
+#include <seqan/seq_io.h>
+#include <seqan/sequence.h>
+
+
+using namespace seqan;
+
+int main() {
+    return 0;
+}
