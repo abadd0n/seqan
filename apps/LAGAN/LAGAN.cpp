6d7df36057dabbb2865e203e71379382202e48f0
Sebastian Proft <sebastianproft@yahoo.de>
2016-04-30 20:15:08 +0200

    LAGAN first try

--- apps/LAGAN/LAGAN.cpp
+++ apps/LAGAN/LAGAN.cpp
@@ -3,13 +3,17 @@
 //
 
 #include <iostream>
+#include <seqan/index.h>
+#include <seqan/seeds.h>
 #include <seqan/seq_io.h>
 #include <seqan/sequence.h>
 
 
 using namespace seqan;
 
-int main(int argc, char *argv[]) {
+//int main(int argc, char *argv[]) {
+int main()
+{
     CharString filePath1 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_6.fa");
     CharString filePath2 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_D.fa");
     SeqFileIn sfi1;
@@ -28,40 +32,52 @@ int main(int argc, char *argv[]) {
     readRecord(meta, d5s1, sfi1);
     readRecord(meta, d5s2, sfi2);
 
-    std::cout << d5s1 << std::endl;
-    std::cout << d5s2 << std::endl;
+    //std::cout << d5s1 << std::endl;
+    //std::cout << d5s2 << std::endl;
 
 
-    unsigned qgramSize = 3;
-    CharString database = "CATGATTACATA";
+    const unsigned qgramSize = 3;
+    Dna5String database = "CATGATTACATA";
 
     //create qgram index with size specified in variable qgramsize
-    typedef Index<DnaString, IndexQGram<UngappedShape<qgramSize> > > TIndex;
+    typedef Index<Dna5String, IndexQGram<UngappedShape<qgramSize> > > TIndex;
+    typedef Infix<Dna5String>::Type TInfix;
+
     TIndex index(d5s1);
-
-    // Default-construct seed.
-    Seed<Simple> seed1;
-    std::cout << "seed1\n"
-    << "beginPositionH == " << beginPositionH(seed1) << "\n"
-    << "endPositionH == " << endPositionH(seed1) << "\n"
-    << "beginPositionV == " << beginPositionV(seed1) << "\n"
-    << "endPositionV == " << endPositionV(seed1) << "\n"
-    << "lowerDiagonal == " << lowerDiagonal(seed1) << "\n"
-    << "upperDiagonal == " << upperDiagonal(seed1) << "\n\n";
-
-    // Construct seed with begin and end position in both sequences.
-    Seed<Simple> seed2(3, 10, 7, 14);
-    setUpperDiagonal(seed2, -7);
-    setLowerDiagonal(seed2, -9);
-    std::cout << "seed2\n"
-    << "beginPositionH == " << beginPositionH(seed2) << "\n"
-    << "endPositionH == " << endPositionH(seed2) << "\n"
-    << "beginPositionV == " << beginPositionV(seed2) << "\n"
-    << "endPositionV == " << endPositionV(seed2) << "\n"
-    << "lowerDiagonal == " << lowerDiagonal(seed2) << "\n"
-    << "upperDiagonal == " << upperDiagonal(seed2) << "\n\n";
+    TInfix kmere;
 
 
+    //create seedSet
+    typedef Seed<Simple> TSeed;
+
+    Score<int, Simple> scoringSchemeAnchor(0, -1, -1);
+    Score<int, Simple> scoringSchemeGap(2, -1, -1, -2);
+
+    String<TSeed> seedChain;
+
+    for (unsigned qPos = 0; qPos < length(d5s2)-qgramSize+1; ++qPos)
+    {
+        kmere = infix(d5s2, qPos, qPos+qgramSize);
+        //std::cout << kmere << std::endl;
+        hash(indexShape(index), begin(kmere));
+        for (unsigned i = 0; i < length(getOccurrences(index, indexShape(index))); ++i)
+            appendValue(seedChain, TSeed(getOccurrences(index, indexShape(index))[i], qPos, qgramSize));
+            //std::cout << getOccurrences(index, indexShape(index))[i] << std::endl;
+    }
+
+    //appendValue(seedChain, TSeed(6, 9, 9, 12));
+    //appendValue(seedChain, TSeed(11, 14, 17, 16));
+
+    Align<Dna5String, ArrayGaps> alignment;
+    resize(rows(alignment), 2);
+    assignSource(row(alignment, 0), d5s1);
+    assignSource(row(alignment, 1), d5s2);
+    AlignConfig<true, false, false, true> alignConfig;
+
+    int result = bandedChainAlignment(alignment, seedChain, scoringSchemeAnchor, scoringSchemeGap, alignConfig, 2);
+
+    std::cout << "Score: " << result << std::endl;
+    std::cout << alignment << std::endl;
 
     return 0;
 }
