2485641c6b3968e1d8f12a19a266130eaa849a1a
Anton Komissarov <akomissarov@zedat.fu-berlin.de>
2016-04-30 18:06:30 +0200

    - data for  testing LAGAN - read that data in

--- apps/LAGAN/LAGAN.cpp
+++ apps/LAGAN/LAGAN.cpp
@@ -10,5 +10,17 @@
 using namespace seqan;
 
 int main() {
+
+    SeqFileIn sfi1(toCString(getAbsolutePath("apps/LAGAN/bovine_adenovirus_6.fa")));
+    SeqFileIn sfi2(toCString(getAbsolutePath("apps/LAGAN/bovine_adenovirus_D.fa")));
+    CharString meta;
+    Dna5String d5s1;
+    Dna5String d5s2;
+    readRecord(meta, d5s1, sfi1);
+    readRecord(meta, d5s2, sfi2);
+
+    std::cout << d5s1 << std::endl;
+    std::cout << d5s2 << std::endl;
+
     return 0;
 }
