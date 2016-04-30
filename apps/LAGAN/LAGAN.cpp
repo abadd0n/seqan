3c4ffaf2efe01f6abf87249611985925ee3655b9
Sebastian Proft <sebastianproft@yahoo.de>
2016-04-30 18:19:48 +0200

    copypasta from seqan tutorial. Trying qgram index

--- apps/LAGAN/LAGAN.cpp
+++ apps/LAGAN/LAGAN.cpp
@@ -9,7 +9,7 @@
 
 using namespace seqan;
 
-int main() {
+int main(int argc, char *argv[]) {
     CharString filePath1 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_6.fa");
     CharString filePath2 = getAbsolutePath("apps/LAGAN/bovine_adenovirus_D.fa");
     SeqFileIn sfi1;
@@ -30,6 +30,38 @@ int main() {
 
     std::cout << d5s1 << std::endl;
     std::cout << d5s2 << std::endl;
+
+
+    unsigned qgramSize = 3;
+    CharString database = "CATGATTACATA";
+
+    //create qgram index with size specified in variable qgramsize
+    typedef Index<DnaString, IndexQGram<UngappedShape<qgramSize> > > TIndex;
+    TIndex index(d5s1);
+
+    // Default-construct seed.
+    Seed<Simple> seed1;
+    std::cout << "seed1\n"
+    << "beginPositionH == " << beginPositionH(seed1) << "\n"
+    << "endPositionH == " << endPositionH(seed1) << "\n"
+    << "beginPositionV == " << beginPositionV(seed1) << "\n"
+    << "endPositionV == " << endPositionV(seed1) << "\n"
+    << "lowerDiagonal == " << lowerDiagonal(seed1) << "\n"
+    << "upperDiagonal == " << upperDiagonal(seed1) << "\n\n";
+
+    // Construct seed with begin and end position in both sequences.
+    Seed<Simple> seed2(3, 10, 7, 14);
+    setUpperDiagonal(seed2, -7);
+    setLowerDiagonal(seed2, -9);
+    std::cout << "seed2\n"
+    << "beginPositionH == " << beginPositionH(seed2) << "\n"
+    << "endPositionH == " << endPositionH(seed2) << "\n"
+    << "beginPositionV == " << beginPositionV(seed2) << "\n"
+    << "endPositionV == " << endPositionV(seed2) << "\n"
+    << "lowerDiagonal == " << lowerDiagonal(seed2) << "\n"
+    << "upperDiagonal == " << upperDiagonal(seed2) << "\n\n";
+
+
 
     return 0;
 }
