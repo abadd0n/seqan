4897951bea3b4effce3dc4af0589f796f1535e0d
Sebastian Proft <sebastianproft@yahoo.de>
2016-05-02 16:59:07 +0200

    create seedChain function

--- apps/LAGAN/LAGAN.cpp
+++ apps/LAGAN/LAGAN.cpp
@@ -15,6 +15,44 @@
 //
 
 using namespace seqan;
+template <typename TSeedChain, typename TSeqString>
+inline bool createSeedChain(TSeedChain & seedChain, TSeqString const & seqH, TSeqString & seqV, unsigned const & qgramSize)
+{
+    //create qgram index with size specified in variable qgramsize
+    typedef Index<Dna5String, IndexQGram<SimpleShape, OpenAddressing > > TIndex;
+    typedef Infix<Dna5String>::Type TInfix;
+
+    TIndex index(seqH);
+    resize(indexShape(index),qgramSize);
+    TInfix kmer;
+
+    //create seedSet
+    typedef Seed<Simple>        TSeed;
+    typedef SeedSet<TSeed>      TSeedSet;
+
+
+    TSeedSet seedSet;
+    Score<int, Simple> scoringScheme( 2,-1,-2 ); // match, mismatch, gap
+    for (unsigned qPos = 0; qPos < length(seqV)-qgramSize+1; ++qPos)
+    {
+        kmer = infix(seqV, qPos, qPos+qgramSize);
+        hash(indexShape(index), begin(kmer));
+        for (unsigned i = 0; i < length(getOccurrences(index, indexShape(index))); ++i) {
+            // add seed to seed set using CHAOS chaining method
+            if ( !addSeed( seedSet, TSeed(getOccurrences( index, indexShape(index) )[i], qPos, qgramSize), 5 /*max diag dist*/, 10 /*band width*/, scoringScheme, seqH, seqV, Chaos() ) )
+                addSeed(seedSet, TSeed(getOccurrences( index, indexShape(index) )[i], qPos, qgramSize), Single()); // just add seed if CHAOS fails
+
+            // Probably we don't need to extend them manually
+            //extendSeed( seed, d5s1, d5s2, EXTEND_BOTH, scoringScheme, 3 /*score drop off*/, UngappedXDrop() );
+            //appendValue(seedChain, TSeed(getOccurrences(index, indexShape(index))[i], qPos, qgramSize));
+        }
+    }
+
+
+    chainSeedsGlobally(seedChain, seedSet, SparseChaining());
+
+    return 1;
+}
 
 //int main(int argc, char *argv[]) {
 int main()
@@ -32,75 +70,32 @@ int main()
         return 1;
     }
     CharString meta;
-    Dna5String d5s1;
-    Dna5String d5s2;
-    readRecord(meta, d5s1, sfi1);
-    readRecord(meta, d5s2, sfi2);
+    Dna5String seqH;
+    Dna5String seqV;
+    readRecord(meta, seqH, sfi1);
+    readRecord(meta, seqV, sfi2);
 
-    //std::cout << d5s1 << std::endl;
-    //std::cout << d5s2 << std::endl;
+    const unsigned qgramSize = 15;
+    String<Seed<Simple>> seedChain;
+
+    createSeedChain(seedChain, seqH, seqV, qgramSize);
 
 
-    const unsigned qgramSize = 30;
-    Dna5String database = "CATGATTACATA";
 
-    //create qgram index with size specified in variable qgramsize
-    typedef Index<Dna5String, IndexQGram<UngappedShape<qgramSize>, OpenAddressing > > TIndex;
-    typedef Infix<Dna5String>::Type TInfix;
-
-    TIndex index(d5s1);
-    TInfix kmer;
-
-    //create seedSet
-    typedef Seed<Simple>        TSeed;
-    typedef SeedSet<TSeed>      TSeedSet;
 
     Score<int, Simple> scoringSchemeAnchor(0, -1, -1);
     Score<int, Simple> scoringSchemeGap(2, -1, -1, -2);
 
-    TSeedSet seedSet;
-    Score<int, Simple> scoringScheme( 2,-1,-2 ); // match, mismatch, gap
-    for (unsigned qPos = 0; qPos < length(d5s2)-qgramSize+1; ++qPos)
-    {
-        kmer = infix(d5s2, qPos, qPos+qgramSize);
-        //std::cout << kmer << std::endl;
-        hash(indexShape(index), begin(kmer));
-        for (unsigned i = 0; i < length(getOccurrences(index, indexShape(index))); ++i) {
-            // add seed to seed set using CHAOS chaining method
-            if ( !addSeed( seedSet, TSeed(getOccurrences( index, indexShape(index) )[i], qPos, qgramSize), 5 /*max diag dist*/, 10 /*band width*/, scoringScheme, d5s1, d5s2, Chaos() ) )
-                addSeed(seedSet, TSeed(getOccurrences( index, indexShape(index) )[i], qPos, qgramSize), Single()); // just add seed if CHAOS fails
-
-            //appendValue(seedChain, TSeed(getOccurrences(index, indexShape(index))[i], qPos, qgramSize));
-            //std::cout << getOccurrences(index, indexShape(index))[i] << std::endl;
-
-            // Probably we don't need to extend them manually
-            //extendSeed( seed, d5s1, d5s2, EXTEND_BOTH, scoringScheme, 3 /*score drop off*/, UngappedXDrop() );
-        }
-    }
-
-    //appendValue(seedChain, TSeed(6, 9, 9, 12));
-    //appendValue(seedChain, TSeed(11, 14, 17, 16));
-    String<TSeed> seedChain;
-
-    chainSeedsGlobally(seedChain, seedSet, SparseChaining());
-
     Align<Dna5String, ArrayGaps> alignment;
     resize(rows(alignment), 2);
-    assignSource(row(alignment, 0), d5s1);
-    assignSource(row(alignment, 1), d5s2);
+    assignSource(row(alignment, 0), seqH);
+    assignSource(row(alignment, 1), seqV);
     AlignConfig<true, true, true, true> alignConfig;
 
     int result = bandedChainAlignment(alignment, seedChain, scoringSchemeAnchor, scoringSchemeGap, alignConfig, 2);
 
     std::cout << "Score: " << result << std::endl;
     std::cout << alignment << std::endl;
-
-    /*
-    typedef Iterator<String<TSeed> > TIter;
-    for ( TIter it = begin(seedChain); it != end(seedChain); ++it ) {
-        std::cout<< *it << std::endl;
-    }
-     */
 
 
     return 0;
